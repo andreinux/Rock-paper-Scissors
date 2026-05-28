f38aa0586a5e39fb85adec42c0ce2dd552b091d7
unknown <andreiacojido@gmail.com>
2026-05-28 23:04:53 +0800

    Logic is Fixed and loop added. However code is messy and some parts are redndant but the logic was performed.

--- main.js
+++ main.js
@@ -19,6 +19,43 @@ function getHumanChoice(input){
 }
 console.log("Player Chooses ",getHumanChoice(humanchoice).toUpperCase());
 
+let hscore = 0;
+let cscore = 0; 
+
+while (hscore < 5 && cscore < 5) {
+    let computerChoice = Math.floor(Math.random() * 3);
+    let humanchoice =(prompt("Enter your choice: ROCK, PAPER, OR SCISSORS"));   
+
+    let input1 = getComputerChoice(computerChoice);
+    let input2 = getHumanChoice(humanchoice);
+
+    let result = playRound(input1, input2);
+    if (result === "You win!"){
+        hscore++;
+    } else if (result === "You lose!"){
+        cscore++;
+    }
+
+console.log("Player Score: ", hscore);
+console.log("Computer Score: ", cscore); 
+
+    if (hscore === 5) {
+        console.log("Player wins the game!");
+    } else if (cscore === 5){
+        console.log("Computer wins the game!");
+    }   
+}
+
+   
+
+
+
+
+
+
+
+
+
 function playRound(input1, input2){
     if (input1 === 0 && input2.toUpperCase() === "ROCK" || input1 === 1 && input2.toUpperCase() === "PAPER" || input1 === 2 && input2.toUpperCase() === "SCISSORS"){
         return "It's a tie!"
@@ -28,4 +65,3 @@ function playRound(input1, input2){
             return "You lose!";
         }
         }
-console.log(playRound(computerChoice, humanchoice));
