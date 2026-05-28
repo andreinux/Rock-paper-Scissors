8dfc0b2b7a87078ead826480de79601774dbc4cd
unknown <andreiacojido@gmail.com>
2026-05-28 22:18:31 +0800

    Fixed The Logic and Result presentation for the user

--- main.js
+++ main.js
@@ -5,18 +5,24 @@ function getComputerChoice(input){
     return input
 }
 
-console.log(getComputerChoice(computerChoice));
+if (computerChoice === 0){
+    console.log("Computer chooses ROCK");
+} else if (computerChoice === 1){
+    console.log("Computer chooses PAPER");
+} else {
+    console.log("Computer chooses SCISSORS");
+}
 
 let humanchoice =(prompt("Enter your choice: ROCK, PAPER, OR SCISSORS"));   
 function getHumanChoice(input){
     return input
 }
-console.log(getHumanChoice(humanchoice));
+console.log("Player Chooses ",getHumanChoice(humanchoice).toUpperCase());
 
 function playRound(input1, input2){
-    if (input1 === input2){
+    if (input1 === 0 && input2.toUpperCase() === "ROCK" || input1 === 1 && input2.toUpperCase() === "PAPER" || input1 === 2 && input2.toUpperCase() === "SCISSORS"){
         return "It's a tie!"
-        } else if (input1 === 0 && input2.toUpperCase() ==="ROCK" || input1 ===1 && input2.toUpperCase() === "PAPER" || input1 ===2 && input2.toUpperCase() === "SCISSORS") {
+        } else if (input1 === 0 && input2.toUpperCase() ==="PAPER" || input1 ===1 && input2.toUpperCase() === "SCISSORS" || input1 ===2 && input2.toUpperCase() === "ROCK") {
             return "You win!"
         } else {
             return "You lose!";
